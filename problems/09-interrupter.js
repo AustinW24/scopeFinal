1de13a6d375bd1dd1b7a320065e3e7f541ca6506
Austin Williams <austinwill264@gmail.com>
2021-04-15 15:27:46 -0700

    09 completed

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -9,14 +9,32 @@ console.log(rudePerson("how are you")); // prints "how what are what you"
 console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
-Invoking the interrupter function again: 
+Invoking the interrupter function again:
 let rudePerson2 = interrupter("yo"); // => returns a function
 console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 
 ***********************************************************************/
 
-// your code here!
+let interrupter = word => {
+  let string = '';
+
+  return function (sentence) {
+    let words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+
+      if (words[i + 1] === undefined) {
+        return string + words[i];
+      } else {
+        string += words[i] + ' ' + word + ' ';
+      }
+    }
+    return string;
+  }
+}
+let rudePerson2 = interrupter("yo"); // => returns a function
+console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
